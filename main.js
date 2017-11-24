7562d23658cb5d713b228cff6b74bc1cbf26e5a8
Ciaron Howell <ciaron_howell@hotmail.co.uk>
2017-11-24 00:02:48 +0000

    Updates

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
+////////////////////// imports //////////////////////////
+//imports discord.js module
 const Discord = require('discord.io');
 
+//imports winston module
 const logger = require('winston');
 
-// gets the token of the bot
-const auth = require('./auth.json');
+// gets token of bot
+const auth = require('./auth.js');
 
-//makes dateFormat available 
-const dateFormat = require('dateformat');
-
-
-//gets recent date
-let now = new Date();
-
-// logger settings
+////////////////////////// logger settings ////////////////////////////////
 logger.remove(logger.transports.Console);
 
 logger.add(logger.transports.Console, {
@@ -22,73 +18,28 @@ logger.add(logger.transports.Console, {
 logger.level = 'debug';
 
 // initialize discord bot
+const client = new Discord.Client();
 
-const bot = new Discord.Client ({
-    token: auth.token,
-    autorun: true
-});
+// logging bot in
+client.login(auth.token);
 
-bot.on('ready', function (evt){
+// prints info
+client.on('ready', () => {
     //shows that the bot is connected and what it is logged in as
     logger.info('Connected');
     logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info(client.username + ' - (' + client.id + ')');
 });
+/////////////////////////////////////////////////////////////////////////////
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-   //this bot will listen for messages starting with '>'
-   if (message.substring(0, 1) == '>') {
-       //stores the argument for the command
-       let args = message.substring(1).split(' ');
-       //will store the command for later use
-       let cmd = args[0]
-       
-       args = args.splice(1);
-       
-       switch(cmd) {
-           //>ping
-           case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-           break;
-           
-           case 'holdtight':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Hold tight, ' + user
-                });
-           break;
-           
-           case '2+2=4':
-                bot.sendMessage({
-                    to: channelID,
-                    message: "That's quick maths, " + user
-                });
-           break;
-           
-           case 'hannah':
-                bot.sendMessage({
-                    to: channelID,
-                    message: "Fortunately, that is " + user 
-                    + "'s girlfriend :smile:"
-                });
-           break;
-           
-           case 'time':
-                bot.sendMessage({
-                    to: channelID,
-                    message: dateFormat(now, 'HH:MM! ') + "Can't touch this!"
-                    });
-           break;
-           
-           case 'date':
-                bot.sendMessage({
-                    to: channelID,
-                    message: "" + dateFormat(now, "dddd, dS mmmm, yyyy")
-                    });
-           break;
-       }
-   }
+//////////////////////////////// test ///////////////////////////////////////
+// the bot will take a message and check it
+client.on('message', msgInfo => {
+    //this bot will listen for '>test'
+    if (message.content == '>test') {
+        // the bot replies
+        message.channel.send("Yo, what's good homie");
+
+    }
 });
+///////////////////////////////////////////////////////////////////////////////
