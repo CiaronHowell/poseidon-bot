de3a22e35e44073b886b325e9e56b4168d5f8347
Ciaron Howell <ciaron_howell@hotmail.co.uk>
2019-02-19 23:20:27 +0000

    Revert "Purge will now work if the user has administrator permissions"

--- commands/moderator/purge.js
+++ commands/moderator/purge.js
@@ -1,17 +1,12 @@
 exports.run = (client, message, args) => {
     // checks if the user is the owner (needs to be changed)
     // TODO: Check if the author has permissions, not the ownerID
-    if (!(message.member.hasPermission('ADMINISTRATOR'))) return;
-
-    // deletes the users message
-    message.delete()
-        .then(console.log('Deleted author message'))
-        .catch(console.error);
+    if (message.author.id !== client.auth.ownerID) return;
 
     // gets a specified amount of messages, and then deletes them
     message.channel.fetchMessages({ limit: args })
         .then(messages => message.channel.bulkDelete(messages))
         .catch(console.error);
 
-    console.log('Delete completed');
+    console.log('Completed');
 };
